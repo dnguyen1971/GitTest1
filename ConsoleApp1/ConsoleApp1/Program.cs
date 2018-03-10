4ddc594e29838b60805a908d8be2236c0ff9aeb8
David Nguyen <dnguyen@dezol.com>
2018-03-10 13:11:28 -0700

    refactored

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -22,12 +22,11 @@ namespace ConsoleApp1
 
         public class Feature1
         {
-            public int Add()
+            public int Add(int x1, int x2)
             {
-                var x1 = 1;
-                var x2 = 2;
-                var sum = x1 + x2;
-                return sum;
+
+                return x1 + x2;
+                 
             }
         }
             
